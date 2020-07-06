2dff1d8722247cc378b1c1ff896f63414010938b
talhkora <martinovicn91@gmail.com>
2020-07-06 12:51:46 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <Header />
+    <AddCharacter v-on:add-char="addChar" />
     <CharacterList />
     <br />
     <PirateCrewList />
@@ -7,15 +9,39 @@
 </template>
 
 <script>
+import Header from "./components/layout/Header.vue";
+import AddCharacter from "./components/AddCharacter.vue";
 import CharacterList from "./components/CharacterList.vue";
 import PirateCrewList from "./components/PirateCrewList.vue";
+import axios from "axios";
 
 export default {
   name: "App",
   components: {
+    Header,
+    AddCharacter,
     CharacterList,
-    PirateCrewList
-  }
+    PirateCrewList,
+  },
+  data() {
+    return {
+      characters: [],
+    };
+  },
+  methods: {
+    addChar(newChar) {
+      const { firstName, lastName, age, devilFruit } = newChar;
+      axios
+        .post("http://localhost:4000/character", {
+          firstName,
+          lastName,
+          age,
+          devilFruit,
+        })
+        .then((res) => (this.characters = [...this.characters, res.data]))
+        .catch((err) => console.log(err));
+    },
+  },
 };
 </script>
 
